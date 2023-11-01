0e2a8ab9a2041fdf171d0812dd189d2b84d3c226
purry03 <rohansh.lko@gmail.com>
2023-11-01 21:25:07 +0530

    added advert placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Container, Grid, SimpleGrid, Skeleton } from '@mantine/core';
+import { Container, Grid, SimpleGrid, Image } from '@mantine/core';
 import WormholeSelect from './components/wormholeSelect/WormholeSelect';
 import { useState } from 'react';
 import MassSelect from './components/massselect/MassSelect';
@@ -15,11 +15,17 @@ export function App() {
 
   const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-sm) / 2)`;
 
+  const child = <Image src='advert.png' height='100%' width='100%' alt='advert image' />
+
   return (
     <Container fluid my="md">
+      <Grid mb='xl'>
+        <Grid.Col span={{ base: 8, xs: 6 }}>{child}</Grid.Col>
+        <Grid.Col span={{ base: 8, xs: 6 }}>{child}</Grid.Col>
+      </Grid>
       <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="xs">
         <Grid gutter="sm">
-          <Grid.Col>
+          <Grid.Col mb='xl'>
             <Container fluid h={SECONDARY_COL_HEIGHT}>
               <WormholeSelect mass={mass} setMass={setMass} modifiedMass={modifiedMass} setModifiedMass={setModifiedMass} />
             </Container>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,36 +1,15 @@
-import { useState } from 'react';
-import { Container, Group, Burger, Title, Divider } from '@mantine/core';
+import { Container, Burger, Title, Divider } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from './header.module.css';
 
-const links = [
-    { link: 'https://github.com/purry03/whcalulator/', label: 'Github' },
-];
 
 export function Header() {
     const [opened, { toggle }] = useDisclosure(false);
-    const [active, setActive] = useState(links[0].link);
-
-    const items = links.map((link) => (
-        <a
-            key={link.label}
-            href={link.link}
-            className={classes.link}
-            data-active={active === link.link || undefined}
-            target='_blank'
-            rel='noreferrer'
-        >
-            {link.label}
-        </a>
-    ));
 
     return (
         <header className={classes.header}>
             <Container fluid size="md" py='md' px={200} className={classes.inner}>
                 <Title c="green" size={28}>Wormhole Mass Calculator</Title>
-                <Group gap={25} visibleFrom="xs">
-                    {items}
-                </Group>
 
                 <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
             </Container>
